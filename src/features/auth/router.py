2177ec8c5aba5922b838f53ed43652ff9486015e
kooflixy <kooflixy@gmail.com>
2026-07-31 17:02:15 +0300

    выдача access tokenа и вход с его помощью

--- src/features/auth/router.py
+++ src/features/auth/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from src.db import async_session_factory
 from src.features.auth.schemas import TokenInfo, UserCreate
 from src.features.auth.security import encode_jwt, hash_password
-from src.features.auth.service import validate_auth_user
+from src.features.auth.service import get_current_auth_user, validate_auth_user
 from src.features.users.repository import UserRepository
 from src.features.users.schemas import UserResponse
 
@@ -26,6 +26,11 @@ async def register(new_user: UserCreate):
 
 @router.post("/login", response_model=TokenInfo, tags=["Auth"])
 async def login(user: UserResponse = Depends(validate_auth_user)):
-    jwt_payload = {"sub": user.id, "username": user.username}
+    jwt_payload = {"sub": str(user.id), "username": user.username}
     token = encode_jwt(jwt_payload)
     return TokenInfo(access_token=token, token_type="Bearer")
+
+
+@router.get("/me", response_model=UserResponse, tags=["Auth"])
+async def get_current_user(user: UserResponse = Depends(get_current_auth_user)):
+    return user
--- src/features/auth/service.py
+++ src/features/auth/service.py
@@ -1,8 +1,13 @@
-from fastapi import Form, HTTPException, status
+from fastapi import Depends, Form, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt import InvalidTokenError
 
 from src.db import async_session_factory
-from src.features.auth.security import validate_password
+from src.features.auth.security import decode_jwt, validate_password
 from src.features.users.repository import UserRepository
+from src.features.users.schemas import UserResponse
+
+http_bearer = HTTPBearer()
 
 
 async def validate_auth_user(username: str = Form(), password: str = Form()):
@@ -18,5 +23,35 @@ async def validate_auth_user(username: str = Form(), password: str = Form()):
 
     if not validate_password(password=password, hashed_password=user.hashed_password):
         raise unauthed_exc
+
+    return user
+
+
+def get_current_token_payload(
+    creds: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> dict:
+    token = creds.credentials
+    try:
+        payload = decode_jwt(token)
+    except InvalidTokenError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="invalid token")
+    return payload
+
+
+async def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+) -> UserResponse:
+    sub_val = payload.get("sub")
+
+    try:
+        user_id = int(sub_val)
+    except (ValueError, TypeError):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="invalid token")
+
+    async with async_session_factory() as session:
+        user = await UserRepository.get_by_id(session, user_id)
+
+    if not user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="invalid token")
 
     return user
