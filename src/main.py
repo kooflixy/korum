677ae959aaf018d24f8c4e2d80d26b8919109d47
kooflixy <kooflixy@gmail.com>
2026-08-17 18:08:19 +0300

    убрал фронтенд:)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 from src.api.router import main_router
 from src.core.config import logging_configure
-from src.frontend import frontend_router
 
 logging_configure()
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/frontend/static"), "static")
 app.include_router(main_router)
-app.include_router(frontend_router)
